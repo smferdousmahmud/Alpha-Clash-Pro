27644fcaca4f1e8319cac8f8e56489c3091939c3
SM Ferdous Mahmud <98792456+smferdousmahmud@users.noreply.github.com>
2026-03-01 16:27:36 +0600

    check

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
